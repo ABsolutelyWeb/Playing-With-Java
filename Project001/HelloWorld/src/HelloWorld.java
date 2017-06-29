7e753f14307371d3b7ad975268fe0df2ac24bbb3
Abhishek Gupta <awittlegupta@gmail.com>
2017-06-29 11:29:18 -0700

    001proj

--- Project001/HelloWorld/src/HelloWorld.java
+++ Project001/HelloWorld/src/HelloWorld.java
@@ -3,6 +3,20 @@
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello there, world!");
+        int world = 1;
+        while (world <= 10){
+            System.out.print("Hello there for the " + world);
+            if (world == 1) {
+                System.out.print("st");
+            }else if (world == 2){
+                System.out.print("nd");
+            }else if (world == 3){
+                System.out.print("rd");
+            }else{
+                System.out.print("th");
+            }
+            System.out.println(" time, world!");
+            world++;
+        }
     }
 }
